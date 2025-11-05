313ffb8087d117c92c9c6758e85fe792e69c7f90
rodrigops123 <rodrigopesilva@outlook.com>
2025-11-04 21:41:06 -0300

    some fixes on mixfunn

--- src/model/models.py
+++ src/model/models.py
@@ -1,13 +1,26 @@
 import torch.nn as nn
 from .mixfunn import Mixfun, Quad
 
+
 class MixFunn(nn.Module):
 
-    def __init__(self, input_=1, output_=1):
+    def __init__(
+        self,
+        input_=1,
+        output_=1,
+        create_parameter=False,
+        n_paramater=1,
+    ):
         super(MixFunn, self).__init__()
         n = 32
         self.layer = nn.Sequential(
-            Mixfun(input_, n, second_order_input=True),
+            Mixfun(
+                input_,
+                n,
+                second_order_input=True,
+                create_parameter=False,
+                n_paramater=1,
+            ),
             # mf.Quad(n,n, second_order = False),
             Quad(n, output_, second_order=False),
         )
